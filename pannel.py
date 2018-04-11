b68ace78e444629c6dec2d01454c9a0af35f47a2
unknown <kanzve@gmail.com>
2018-04-11 15:51:48 +0800

    add panel

---
+++ pannel.py
@@ -0,0 +1,11 @@
+import aritbasket_option_pricer as aop
+
+if __name__ == '__main__':
+    aopx=aop.aritbasket_opt('c',100,100,3,80,0.05,0.3,0.3,0.5)
+    aopx.value()
+    aopx.value()
+    aopx.value()
+    aopx.value()
+    aopx.value()
+    aopx.value()
+    aopx.value()
