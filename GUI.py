17979ef58ad64afba4685d39693a5dbd4a4d498f
cj-d3v <jian.chen.dev@outlook.com>
2018-04-18 23:33:45 +0800

    change a litte UI

--- GUI.py
+++ GUI.py
@@ -602,46 +602,56 @@ def arithbCal(text1, text2, typ, asset_price1, asset_price2, mature_time, strike
 
 win = tk.Tk()
 win.title("Assigment 3")
+win.config(bg='ghost white')
+win.geometry("400x380") 
+win.resizable(0, 0) 
 
 text0 = tk.Label(win, text='European Option Pricer:', font=("Helvetica", 16), height=2)
 text0.grid(row=0)
-button0 = tk.Button(win, text="Open", font=("Helvetica", 15), command=euroWin)
+text0.config(bg='ghost white')
+button0 = tk.Button(win, text="Open", font=("Helvetica", 13), command=euroWin, relief='groove')
 button0.grid(row=0, column=1)
 
 
 text1 = tk.Label(win, text='Implied Volatility Calculation:', font=("Helvetica", 16), height=2)
 text1.grid(row=1)
-button1 = tk.Button(win, text="Open", font=("Helvetica", 15), command=ivWin)
+text1.config(bg='ghost white')
+button1 = tk.Button(win, relief='groove',  text="Open", font=("Helvetica", 13), command=ivWin)
 button1.grid(row=1, column=1)
 
 
 text2 = tk.Label(win, text='Binary Tree Option Pricer:', font=("Helvetica", 16), height=2)
 text2.grid(row=2)
-button2 = tk.Button(win, text="Open", font=("Helvetica", 15), command=biWin)
+text2.config(bg='ghost white')
+button2 = tk.Button(win, relief='groove',  text="Open", font=("Helvetica", 13), command=biWin)
 button2.grid(row=2, column=1)
 
 
 text3 = tk.Label(win, text='Geometirc Asian Option Pricer:', font=("Helvetica", 16), height=2)
 text3.grid(row=3)
-button3 = tk.Button(win, text="Open", font=("Helvetica", 15), command=geoAsianWin)
+text3.config(bg='ghost white')
+button3 = tk.Button(win, relief='groove',  text="Open", font=("Helvetica", 13), command=geoAsianWin)
 button3.grid(row=3, column=1)
 
 
 text4 = tk.Label(win, text='Arithmetic Asian Option Pricer:', font=("Helvetica", 16), height=2)
 text4.grid(row=4)
-button4 = tk.Button(win, text="Open", font=("Helvetica", 15), command=arithAsianWin)
+text4.config(bg='ghost white')
+button4 = tk.Button(win, relief='groove',  text="Open", font=("Helvetica", 13), command=arithAsianWin)
 button4.grid(row=4, column=1)
 
 
 text5 = tk.Label(win, text='Geometirc Basket Option Pricer:', font=("Helvetica", 16), height=2)
 text5.grid(row=5)
-button1 = tk.Button(win, text="Open", font=("Helvetica", 15), command=geoBaskWin)
+text5.config(bg='ghost white')
+button1 = tk.Button(win, relief='groove',  text="Open", font=("Helvetica", 13), command=geoBaskWin)
 button1.grid(row=5, column=1)
 
 
 text6 = tk.Label(win, text='Arithmetirc Basket Option Pricer:', font=("Helvetica", 16), height=2)
 text6.grid(row=6)
-button6 = tk.Button(win, text="Open", font=("Helvetica", 15), command=arithBaskWin)
+text6.config(bg='ghost white')
+button6 = tk.Button(win, relief='groove',  text="Open", font=("Helvetica", 13), command=arithBaskWin)
 button6.grid(row=6, column=1)
 
 
